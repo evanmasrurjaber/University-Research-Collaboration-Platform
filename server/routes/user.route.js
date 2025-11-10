feb542e28f62cb7b114b2d99388f91ad83ff4f41
evanmasrurjaber <evanmasrurjaber@gmail.com>
2025-11-10 17:40:32 +0600

    debug

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -7,7 +7,7 @@ import singleUpload from '../middlewares/multer.js';
 const router = express.Router();
 
 router.route('/register').post(register);
-router.route('/login').post(login);
+router.route('/login').post((req, res, next) => { console.log('login body:', req.body); next(); });
 router.route('/logout').post(isAuthenticated, logout);
 router.route('/profile/update').post(isAuthenticated, singleUpload, updateProfile);
 router.route('/theme').get(isAuthenticated, getTheme);
