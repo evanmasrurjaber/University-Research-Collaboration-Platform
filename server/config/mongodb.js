be81d7141fdd6f508d9662f831a883b100c347d4
evanmasrurjaber <evanmasrurjaber@gmail.com>
2025-11-10 20:36:55 +0600

    database fix

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     mongoose.connection.on('connected', () => console.log('Database connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/URCP`, );
+    await mongoose.connect(`${process.env.MONGODB_URI}/test`, );
 };
 
 export default connectDB;
